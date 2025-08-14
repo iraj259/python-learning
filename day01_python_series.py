7faf38101607931c0c4e9c304f1c3478c78ee21d
iraj259 <irajj.259@gmail.com>
2025-08-14 22:07:15 +0100

    Day 01

---
+++ day01_python_series.py
@@ -0,0 +1,10 @@
+# print("Pak","Ind","Sri",sep='@')
+# print("Hello",end=' ' )
+# print("World")
+# infinite
+# print(1e309) 
+
+# input("write something: ")
+# if u dont want to assign any value to the variable but want to keep it in the code so
+k=None
+print(k)
