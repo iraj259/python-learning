cc944827ea9e5e358a92eb77c8f306ca28c64a29
iraj259 <irajj.259@gmail.com>
2025-08-15 23:02:57 +0100

    Day 02

--- day02_python_series.py
+++ day02_python_series.py
@@ -1,5 +1,17 @@
-number = int(input("Enter the number you want a table of: "))
-i =1 
-while i < 10:
-    print(number * i)
-    i = i + 1
+# number = int(input("Enter the number you want a table of: "))
+# i =1 
+# while i < 10:
+#     print(number * i)
+#     i = i + 1
+
+import random
+
+prediction = random.randint(1,10)
+user = int(input("Enter a number "))
+while prediction != user:
+    if prediction < user:
+        print("Your number is too high")
+    else:
+        print("Your number is too low")
+    user = int(input("Enter again "))
+print("You Won!")
