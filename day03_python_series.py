1a87e34807dee7baf1b842efc8df18898ebfca8b
iraj259 <irajj.259@gmail.com>
2025-08-16 13:51:20 +0100

    Day 03

--- day03_python_series.py
+++ day03_python_series.py
@@ -1,5 +1,12 @@
+# rows = int(input("Enter the number of rows: "))
+# for i in range(rows):
+#     for j in range(i+1):
+#         print("* ", end="")
+#     print("\n")
+
+
 rows = int(input("Enter the number of rows: "))
-for i in range(rows):
-    for j in range(i+1):
-        print("* ", end="")
-    print("\n")
+for i in range(rows, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
