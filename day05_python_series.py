ea5056e41afc7969fa7deb507782acca3a661104
iraj259 <irajj.259@gmail.com>
2025-08-18 17:54:21 +0100

    Day 05

--- day05_python_series.py
+++ day05_python_series.py
@@ -6,10 +6,17 @@
 # myList=[1,4,5,3,5]
 # random.shuffle(myList)
 # print(myList)
-import time
-print(time.time())
-print(time.ctime())
+# import time
+# print(time.time())
+# print(time.ctime())
 
-import os
-print(os.getcwd())
-print(os.listdir())
+# import os
+# print(os.getcwd())
+# print(os.listdir())
+
+c='hello'
+d=" 'hi' "
+e='''hi
+hello
+hi'''
+print(c,d,e)
