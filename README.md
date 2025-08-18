c5104f446335ec674afa58c2f4049868e0bf58d7
iraj259 <irajj.259@gmail.com>
2025-08-18 17:48:25 +0100

    Day 05

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Day 2 was all about conditionals and loops.
 Day 3 was all about nested loops.
 
 Day 4 was all about break, continue statements and built in functions.
+
+Day 5 was all about built in modules
---
+++ day05_python_series.py
@@ -0,0 +1,15 @@
+# import math
+# print(math.sqrt(3))
+
+# import random
+# print(random.randint(1,10))
+# myList=[1,4,5,3,5]
+# random.shuffle(myList)
+# print(myList)
+import time
+print(time.time())
+print(time.ctime())
+
+import os
+print(os.getcwd())
+print(os.listdir())
