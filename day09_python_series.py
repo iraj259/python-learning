e4cd768b5b3934b21d28b760345ff64170d1904b
iraj259 <irajj.259@gmail.com>
2025-08-22 22:36:13 +0100

    Day 09

--- day09_python_series.py
+++ day09_python_series.py
@@ -1,3 +1,16 @@
 def greet():
     print('hello')
 greet()
+
+def greet(name):
+    print('hello', name)
+greet('iraj')
+
+def add(a,b):
+    return a+b
+print(add(5,5))
+
+def greet(name='guest'):
+    print(f"hello, {name}")
+greet('')
+greet('iraj')
