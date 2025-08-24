46901ec7dc8295849b98349f2fe0f5cd5ec00df8
iraj259 <irajj.259@gmail.com>
2025-08-24 22:05:53 +0100

    Day 11

--- day_11_python_series.py
+++ day_11_python_series.py
@@ -1,23 +1,33 @@
-def multiply(a,b):
-    result = 0
-    for i in range(b):
-     result = result + a
-    print(result)
-multiply(3,4)
+# def multiply(a,b):
+#     result = 0
+#     for i in range(b):
+#      result = result + a
+#     print(result)
+# multiply(3,4)
 
 # recursion
-def mul(a,b):
-   if b==1:
-      return a
-   else:
-      return a+mul(a,b-1)
-print(mul(3,4))
+# def mul(a,b):
+#    if b==1:
+#       return a
+#    else:
+#       return a+mul(a,b-1)
+# print(mul(3,4))
 
-def fact(number):
-   if number ==1:
-      return 1
-   else:
-      return number * fact(number-1)
+# def fact(number):
+#    if number ==1:
+#       return 1
+#    else:
+#       return number * fact(number-1)
    
-print(fact(5))
+# print(fact(5))
 
+
+def palin(text):
+    if len(text)<=1:
+        print("Palindrome")
+    else:
+        if text[0] == text[-1]:
+            palin(text[1:-1])
+        else:
+            print('not palin')
+print(palin('madam'))
