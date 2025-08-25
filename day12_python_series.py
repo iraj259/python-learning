e082c68ca4c1a642d5f15d78163dbf6facf6bb62
iraj259 <irajj.259@gmail.com>
2025-08-25 21:49:44 +0100

    Day 12

---
+++ day12_python_series.py
@@ -0,0 +1,43 @@
+# practice 1
+# a = int(input("Enter age 1: "))
+# b = int(input("Enter age 2: "))
+# c = int(input("Enter age 3: "))
+
+# if a < 0 or b < 0 or c < 0:
+#     print('Invalid age')
+# elif a == b and b == c:
+#     print(f'All ages are equal i.e {a}')
+# elif a >= b and a >= c:
+#     print('Age 1 is the oldest')
+# elif b >= a and b >= c:
+#     print(f'Age 2 is the oldest i.e {b}')
+# else:
+#     print(f'Age 3 is the oldest i.e {c}')
+
+# practice 2
+# a = int(input('Enter the first value '))
+# b = int(input('Enter the second value '))
+# c =0
+# print (f'a before swap is {a} and b is {b}')
+# c=a
+# a=b
+# b=c
+# print(f'a is now {a} and b is now {b}')
+
+# practice 3
+# a = int(input('Enter the first value '))
+# b = int(input('Enter the second value '))
+# print (f'a before swap is {a} and b is {b}')
+# a,b = b,a
+# print(f'a is now {a} and b is now {b}')
+
+# practice 4
+# a=int(input('enter the first number '))
+# b=int(input('enter the 2nd number '))
+# c=int(input('enter the third number '))
+# print(a+b+c)
+
+# practice 5
+a = 1234
+reverse=int(str(a)[::-1])
+print(reverse)
